3c74786188f8c0220724ea48316bf97dda47acf2
Muhamad Rizal Erwiansyah <rizal@goorita.com>
2022-08-17 18:02:54 +0700

    create API

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -3,9 +3,79 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProjectController extends Controller
 {
-    //
+    public function access_project(Request $request)
+    {
+        try {
+            $project =  DB::table('projects')
+                ->where('projects.unique_field', '=', $request->code)
+                ->first();
+            $data['message'] = 'Berhasil';
+            $data['status'] = 200;
+            $data['data'] = $project;
+        } catch (\Exception $ex) {
+            $data['message'] = $ex->getMessage();
+            $data['status'] = 500;
+        }
+        return $data;
+    }
+
+    public function access_screen_by_code_project(Request $request)
+    {
+        try {
+            $project =  DB::table('projects')
+                ->join('screens', 'screens.project_id', '=', 'projects.id')
+                ->select('screens.*')
+                ->where('projects.unique_field', '=', $request->code)
+                ->get();
+            $data['message'] = 'Berhasil';
+            $data['status'] = 200;
+            $data['data'] = $project;
+        } catch (\Exception $ex) {
+            $data['message'] = $ex->getMessage();
+            $data['status'] = 500;
+        }
+        return $data;
+    }
+
+    public function getItemByScreen(Request $request)
+    {
+        try {
+            $project =  DB::table('screens')
+                ->join('items', 'items.screen_id', '=', 'screens.id')
+                ->select('items.*')
+                ->where('screens.id', '=', $request->screen_id)
+                ->get();
+            $data['message'] = 'Berhasil';
+            $data['status'] = 200;
+            $data['data'] = $project;
+        } catch (\Exception $ex) {
+            $data['message'] = $ex->getMessage();
+            $data['status'] = 500;
+        }
+        return $data;
+    }
+
+    public function getParticipantByItemID(Request $request)
+    {
+        try {
+            $project =  DB::table('items')
+                ->join('participants', 'participants.item_id', '=', 'items.id')
+                ->select('participants.*')
+                ->where('items.id', '=', $request->item_id)
+                ->get();
+            $data['message'] = 'Berhasil';
+            $data['status'] = 200;
+            $data['data'] = $project;
+        } catch (\Exception $ex) {
+            $data['message'] = $ex->getMessage();
+            $data['status'] = 500;
+        }
+        return $data;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,10 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::get('/', function () {
     return "GOORITA API";
 });
 Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+Route::post('akses-project', [ProjectController::class, 'access_project']);
+Route::post('akses-screen-by-code', [ProjectController::class, 'access_screen_by_code_project']);
+Route::post('item-by-screen-id', [ProjectController::class, 'getItemByScreen']);
+Route::post('get-participant-by-item', [ProjectController::class, 'getParticipantByItemID']);
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    
 });
